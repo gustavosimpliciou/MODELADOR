186375ed154688704ce893b059bba672bf02a5d7
Gustavo Simplício <AGENESEEZ@GMAIL.COM>
2026-09-26 01:54:39 -0300

    sens200

--- cortes/components/layout/left-panel.tsx
+++ cortes/components/layout/left-panel.tsx
@@ -103,7 +103,7 @@ export function LeftPanel() {
               <input
                 type="range"
                 min={1}
-                max={250}
+                max={200}
                 step={1}
                 value={sharpAngle}
                 onChange={(e) => setSharpAngle(Number(e.target.value))}
